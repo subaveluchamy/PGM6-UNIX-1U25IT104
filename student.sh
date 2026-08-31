11ad370677fb2104a05c29bc305e3219dff1cbf3
subaveluchamy <subaveluchamy0608@gmail.com>
2026-08-31 12:39:50 +0530

    PGM 11 UNIX 1U25IT104

--- student.sh
+++ student.sh
@@ -1,6 +1,6 @@
-cat>new file.txt
+cat > new file.txt
 cat filename.txt
-cat file1.txt file2.txt
+cat file1.txt file2.txt > combined file.txt
 cat file.txt >> existing file.txt
 cat -n filename.txt
-ls
+
